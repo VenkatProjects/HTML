3951a56db8197da50f0af1c5db8b1a2f854480cc
venkatprojects <esan1610@gmail.com>
2020-07-21 19:41:37 +0530

    login page created

---
+++ js.js
@@ -0,0 +1,12 @@
+document.write("<h2><center>Basic concepts of javascript</center></h2>")    
+
+function addNumber(a,b)
+{
+    /*var a = 5;
+    var b = 10;*/
+    var total = a+b;
+    return total;
+    document.write("<h2>the total is :"+total+"</h2>");
+} 
+var output = addNumber("venkat","esan");
+document.write("<h2>the output name is :"+output+"</h2>");
